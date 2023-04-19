a3198924e62bca66d362e09282a6c0ed45456814
wideRuledAZ <danielmartinez0603@gmail.com>
2023-04-18 21:46:10 -0500

    4/18 10 pm commit daniel

--- app.py
+++ app.py
@@ -29,26 +29,30 @@ swipes = db.get_collection("Fake_Data")
 
 daysOpen =  {
         "Rec" :
-            {"Mon":[6,23], "Tues":[6,23], "Wed": [6,23], "Thurs":[6,23], "Fri":[6,22], "Sat":[8,20], "Sum":[12,23]},
+            {"Mon":[6,23], "Tues":[6,23], "Wed": [6,23], "Thurs":[6,23], "Fri":[6,22], "Sat":[8,20], "Sun":[12,23]},
         "Rand" :
             {"Mon":[7,15], "Tues":[7,15], "Wed":[7,15], "Thurs":[7,15], "Fri":[7,15]},
         "Roth" :
-            {"Mon":[7,20], "Tues":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sum":[9,20]},
+            {"Mon":[7,20], "Tues":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sun":[9,20]},
         "Zeppos" :
-            {"Mon":[7,20], "Tues":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sum":[9,20]},
+            {"Mon":[7,20], "Tues":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sun":[9,20]},
         "Central" :
-            {"Mon":[8,24], "Tues":[8,24], "Wed":[8,24], "Thurs":[8,24], "Fri":[8,24], "Sat":[8,24], "Sum":[8,24]},
+            {"Mon":[8,24], "Tues":[8,24], "Wed":[8,24], "Thurs":[8,24], "Fri":[8,24], "Sat":[8,24], "Sun":[8,24]},
         "Commons" :
-            {"Mon":[7,20], "Tues":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sum":[9,20]},
+            {"Mon":[7,20], "Tues":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sun":[9,20]},
         "EBI" :
-            {"Mon":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sum":[9,20]},
-}
+            {"Mon":[7,20], "Wed":[7,20], "Thurs":[7,20], "Fri":[7,20], "Sat":[9,20], "Sun":[9,20]},
+        "Alumni" : 
+            {"Mon":[6,23], "Tues":[6,23], "Wed": [6,23], "Thurs":[6,23], "Fri":[6,22], "Sat":[8,20], "Sun":[12,23]},
+            }
+
+hours = ['8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','10AM','11AM', '12AM', '1AM']
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/getDataByLocation/<string:locationID>')
+@app.route('/api/getDataByLocation/<string:locationID>')
 def getDataByLocation(locationID):
     data_json = []
     data = swipes.find({"locationID": locationID})
@@ -77,13 +81,13 @@ def testDate():
 
     return str(ts)
 
-@app.route('/getLocationType/<string:locationID>')
+@app.route('/api/getLocationType/<string:locationID>')
 def getLocationType(locationID):
     if (locationID == "Rec" or locationID == "Alumni"):
         return "gym"
     return "dining"
 
-@app.route('/getCurrentOccupancy/<string:locationID>')
+@app.route('/api/getCurrentOccupancy/<string:locationID>')
 def getCurrentOccupancy(locationID):
     from_date = datetime.datetime.now() - timedelta(hours = 1)
     to_date = datetime.datetime.now()
@@ -91,9 +95,9 @@ def getCurrentOccupancy(locationID):
         {"locationID": locationID, "Timestamp": {"$gte": from_date, "$lt": to_date}}
     )
     
-    return json.dumps({"currentOccupancy":occupancy})
+    return json.dumps(occupancy)
 
-@app.route('/getOccupancyByHour/<string:locationID>/<string:hour>')
+@app.route('/api/getOccupancyByHour/<string:locationID>/<string:hour>')
 def getOccupancyByHour(locationID, hour):
     time = datetime.strptime(hour, "%Y-%m-%dT%H:%M:%S.%f%z")
     ts = time.timestamp()
@@ -109,9 +113,8 @@ def getOccupancyByHour(locationID, hour):
     return str(ts)
 
 # Not right implementation, can someone help me with this?  
-@app.route('/getAverageOccupancyByHourOnWeekday/<string:locationID>/<string:weekday>/<int:hour>')
+@app.route('/api/getAverageOccupancyByHourOnWeekday/<string:locationID>/<string:weekday>/<int:hour>')
 def getAverageOccupancyByHourOnWeekday(locationID, weekday, hour):
-
     swipes_by_location_weekday_hour = swipes.find(
         {
             "locationID": locationID,
@@ -145,7 +148,7 @@ def getAverageOccupancyByHourOnWeekday(locationID, weekday, hour):
     avg_occupancy = int(count / num_days)
     return avg_occupancy
 
-@app.route('/getAverageOccupancyByWeekday/<string:locationID>/<string:weekday>')
+@app.route('/api/getAverageOccupancyByWeekday/<string:locationID>/<string:weekday>')
 def getAverageOccupancyByWeekday(locationID, weekday):
     data_json = []
 
@@ -155,11 +158,11 @@ def getAverageOccupancyByWeekday(locationID, weekday):
 
         for i in range(open + 1, close + 1):
             avg = getAverageOccupancyByHourOnWeekday(locationID, weekday, i)
-            data_json.append({i : avg})
+            data_json.append({"time" : hours[i - 8], "occupancy" : avg})
 
     return json.dumps(data_json)
 
-@app.route('/getAllAverageOccupancies/<string:locationID>')
+@app.route('/api/getAllAverageOccupancies/<string:locationID>')
 def getAllAverageOccupancies(locationID):
     data_json = []
     days = daysOpen[locationID]
@@ -173,8 +176,28 @@ def getAllAverageOccupancies(locationID):
 
     return json.dumps(data_json)
 
+@app.route('/api/getAllTotalOccupancies/<string:locationID>')
+def getAllTotalOccupancies(locationID):
+    data_json = []
+    days = daysOpen[locationID]
+
+
+    if days is None:
+        return json.dumps(data_json)
+
+    for day in days:
+        count = swipes.count_documents(
+        {
+            "locationID": locationID,
+            "Weekday": day,
+            }
+        )
+        data_json.append({"weekday": day, "occupancy": count})
+
+    return data_json
+
 # Test call below:
-# print(getAverageOccupancyByHourOnWeekday('Rec', 'Sum', 23))
+# print(getAverageOccupancyByHourOnWeekday('Rec', 'Sun', 23))
 
 # getCurrentOccupancy(location_id)
 # getOccupancyByHour(locationID, hour)
